08f861f410c207963c812e51328f771a6d2cafdb
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 14:17:44 -0700

    adding problem regarding garbage collection

--- src/main/java/ocaprep/ch01/Rabbit.java
+++ src/main/java/ocaprep/ch01/Rabbit.java
@@ -1,18 +1,27 @@
 package ocaprep.ch01;
 
-import java.util.*;
-
 public class Rabbit {
 	/*
-	 * Review Question 18
-	 * 
+	 * Review Question 19
 	 */
+	// which of the following statements are true
 
-	/*
-	 * order of packages and import statements
-	 * --> package 
-	 * --> import 
-	 * --> public class ThisIsAClass{ }
-	 */
+	public static void main(String[] args) {
+      Rabbit one = new Rabbit();
+      Rabbit two = new Rabbit();
+      Rabbit three = one;
 
+      one = null; // garbage collected because one is dereferenced
+
+      Rabbit four = one; // garbage collected because four is referencing a null value
+      
+      three = null; // garbage collected because three is referencing a null value
+      
+      two = null; // garbage collected because two is referencing a null value
+
+      two = new Rabbit();
+      
+      System.gc();
+
+	}
 }
---
+++ src/main/java/ocaprep/ch01/ReviewQuestion18.java
@@ -0,0 +1,14 @@
+package ocaprep.ch01;
+
+import java.util.*;
+
+public class ReviewQuestion18{
+
+	/*
+	 * order of packages and import statements
+	 * --> package 
+	 * --> import 
+	 * --> public class ThisIsAClass{ }
+	 */
+
+}

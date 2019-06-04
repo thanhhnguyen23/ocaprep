430d1c0bc5ead1f025826a6364661229a879d425
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 14:53:48 -0700

    editing formating for chapter 1

--- src/main/java/ocaprep/ch01/ReviewQuestion13.java
+++ src/main/java/ocaprep/ch01/ReviewQuestion13.java
@@ -7,16 +7,16 @@ public class ReviewQuestion13 {
 	 */
 
 	/* 
-	 * instance variables of type boolean defaults to false // true 
-	 * instance variables of type boolean defaults to true // false 
-	 * instance variables of type boolean defaults to null // false
+	 * instance variables of type boolean defaults to false 	// true 
+	 * instance variables of type boolean defaults to true 		// false 
+	 * instance variables of type boolean defaults to null 		// false
 	 */
 	boolean myBoolean;
 	
 	/*
-	 * instance variable of type int defaults to 0 // true 
-	 * instance variable of type int defaults to 0.0 // false 
-	 * instance variable of type int defaults to null // false
+	 * instance variable of type int defaults to 0 		// true 
+	 * instance variable of type int defaults to 0.0	// false 
+	 * instance variable of type int defaults to null 	// false
 	 */
 	int myInt;
 
--- src/main/java/ocaprep/ch01/ReviewQuestion15.java
+++ src/main/java/ocaprep/ch01/ReviewQuestion15.java
@@ -9,11 +9,11 @@ public class ReviewQuestion15 {
 		 * _ (underscores) are only valid within digits 1_234_678
 		 * 		> leading _ or trailing _ are will not compile
 		 */
-		int i1 = 1_234; // will compile
-//      double d1 = 1_234_.0; // will NOT compile // underscores must be within digits
-//      double d2 = 1_234._0; // will NOT compile // underscores must be within digits
-//      double d3 = 1_234.0_; // will NOT compile // underscores must be within digits
-		double d4 = 1_234.0; // will compile
+		int i1 = 1_234; 				// will compile
+//      double d1 = 1_234_.0; 			// will NOT compile // underscores must be within digits
+//      double d2 = 1_234._0; 			// will NOT compile // underscores must be within digits
+//      double d3 = 1_234.0_; 			// will NOT compile // underscores must be within digits
+		double d4 = 1_234.0; 			// will compile
 	}
 
 }

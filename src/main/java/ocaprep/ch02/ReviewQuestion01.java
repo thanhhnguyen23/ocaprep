d3dbde1d7445060a7383c107e374656cab5ebb71
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 16:01:39 -0700

    reviewing boolean expressions

--- src/main/java/ocaprep/ch02/ReviewQuestion01.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion01.java
@@ -6,20 +6,42 @@ public class ReviewQuestion01 {
 	 * which of the following Java operators can be used with boolean variables?
 	 */
 
-	/*
-	 * == works
-	 * + DOES NOT WORK
-	 * -- DOES NOT WORK
-	 * ! // != works
-	 * % DOES NOT WORK
-	 * <= works
-	 */
+//	 == 		works
+//	 + 			DOES NOT WORK
+//	 -- 		DOES NOT WORK
+//	 ! 			!= works
+//	 % 			DOES NOT WORK
+//	 <= 		works
+
 	public static void main(String[] args) {
 		/*
 		 * examples of boolean expressions
+		 * 		> boolean expressions must have comparison factor to be a valid boolean expression
 		 */
-		boolean myBoolean = false;
-		
-	}
+      boolean b1 = true;
+      boolean b2 = false;
 
+      if(b1 == b2){
+          System.out.println("== operator works");
+      }
+//      if(b1 + b2){ // invalid boolean expression
+//          System.out.println("+ operator works");
+//      }
+//      if(b1 -- b2){ // invalid boolean expression
+//          System.out.println("-- operator works");
+//      }
+      if(b1 != b2){
+          System.out.println("!= operator works");
+      }
+//      if(b1 % b2){ // invalid boolean expression
+//          System.out.println("% operator works");
+//      }
+//      if(b1 <= b2){ // invalid boolean expression
+//          System.out.println("<= operator works");
+//      }
+      else{
+          System.out.println("something went wrong");
+      }
+
+	}
 }

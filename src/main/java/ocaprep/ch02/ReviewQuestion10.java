1037af18e3fa2c35c389dd1ba8c54f06689d50c8
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 21:32:28 -0700

    reviewing implicit type conversion where byte data types are automatically promoted for arithmetic operations

--- src/main/java/ocaprep/ch02/ReviewQuestion10.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion10.java
@@ -1,5 +1,13 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion10 {
+    /*
+     * what is the output of the following code?
+     */
 
+    public static void main(String[]args){
+        byte a = 40, b = 50;
+//        byte sum = (byte) a + b; // will not compile; type mismatch from int to byte; data types will be automatically promoted to int for arithmetic operation
+//        System.out.println(sum);
+    }
 }

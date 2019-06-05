0bbc378727357a5effedf799e8086a8c1ee4c549
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 21:11:47 -0700

    reviewing multiple variable declarations can be written as a one liner

--- src/main/java/ocaprep/ch02/ReviewQuestion08.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion08.java
@@ -1,5 +1,15 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion08 {
-
+	/*
+	 * what is the output of the following code snippet?
+	 */
+	public static void main(String[] args) {
+		/*
+		 * each subsequent variable can be declared with the same data type on the same line separated with a comma
+		 */
+		boolean x = true, z = true;
+		int y = 20;
+		System.out.println(x + ", " + y + ", " + z); // x: true, y: 20, z: true
+	}
 }

d9d8dbcdfb9f040105b3c18699047edd22f73c77
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 21:06:08 -0700

    reviewing ternary expressions

--- src/main/java/ocaprep/ch02/ReviewQuestion07.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion07.java
@@ -1,5 +1,21 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion07 {
+	/*
+	 * what is the output of the following code?
+	 */
+	public static void main(String[] args) {
+		int x = 5;
+//        5 > 2 --> true
+//        next statement
+//        5 < 4 --> false
+//        resulting in 8
+//        System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7); // will compile; output: 8
 
+//		another example
+        System.out.println( x > 2 ? x < 4 ? "California" : "Oregon" : "Washington"); 
+        // 5 > 2 --> true
+        // 5 < 4 --> false
+        // returns --> Oregon
+	}
 }

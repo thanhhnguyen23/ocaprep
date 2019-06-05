7d8de763db9d84cf666e83b512426943c641529b
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 21:27:36 -0700

    reviewing infinite loops with for-loops

--- src/main/java/ocaprep/ch02/ReviewQuestion09.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion09.java
@@ -1,5 +1,17 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion09 {
+	/*
+	 * how many times will the following code print "Hello World"?
+	 */
+	public static void main(String[] args) {
 
+//		for (int i = 0; i < 10;) { // without i++ will cause infinite loop
+		for (int i = 0; i < 10; i++) { // incrementer needs to be in the conditional statement
+//			i = i++; // looks like it doesn't alter behavior
+//			System.out.println("iteration: " + i);
+			System.out.println("Hello World"); // output: 10 times Hello World starting at 0
+		}
+
+	}
 }

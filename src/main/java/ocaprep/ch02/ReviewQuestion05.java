b27970f9a6bb3c879ca5bd276a9e94cd7f9803da
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 20:37:28 -0700

    reviewing interfaces/classes

--- src/main/java/ocaprep/ch02/ReviewQuestion05.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion05.java
@@ -1,5 +1,24 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion05 {
+	/*
+	 * what is the output of the following code snippet?
+	 */
+    public static void main(String[]args){
+    	/* 
+    	 * List<Integer> is child interface of the Collection API 
+    	 * ArrayList<Integer> is implementation class of List 
+    	 * 
+    	 * should declare most general type, in this case it's the List interface
+    	 */
+        java.util.List<Integer> list = new java.util.ArrayList<Integer>();
 
+        list.add(10);
+        list.add(14);
+
+        for(int x : list){
+            System.out.print(x + ", "); // output: 10, 
+            break;
+        }
+    }
 }

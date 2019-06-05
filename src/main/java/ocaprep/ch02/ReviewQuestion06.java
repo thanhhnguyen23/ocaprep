b606036f2b88e9e5d3115b958d20468e439e66a0
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-04 20:49:49 -0700

    reviewing integral types and correct syntax

--- src/main/java/ocaprep/ch02/ReviewQuestion06.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion06.java
@@ -1,5 +1,18 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion06 {
+	/*
+	 * what is the output of the following code snippet?
+	 */
+	public static void main(String[]args){
+		// integral types
+		// byte -> short -> int -> long -> float -> double 
+        int x = 4;
+        long y = x * 4 - x++; // will compile; int converts to long implicitly
 
+        if(y < 10) System.out.println("Too Low");
+        
+        else System.out.println("Just right");
+//        else System.out.println("Too High"); // will not compile; syntax error
+    }
 }

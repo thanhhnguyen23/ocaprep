1fadbb63488ba7c48f267c85cc2327b1fd396277
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-07 10:22:56 -0700

    switch statements, char, break statements

--- src/main/java/ocaprep/ch02/ReviewQuestion20.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion20.java
@@ -1,5 +1,29 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion20 {
+	/*
+	 * what is the result of the following code snippet?
+	 */
 
+	public static void main(String[] args) {
+
+		final char a = 'A', d = 'D';
+		char grade = 'B'; // 'B' and B are different and compiler is able to distinguish between the two
+
+		switch (grade) {
+		case a:
+		case 'B':
+			// no break statement so case falls into the next switch statement
+			System.out.print("great"); // output: greatgood
+		case 'C':
+			System.out.print("good");
+			/*
+			 * will break out of the switch cases; otherwise compiler will go through each switch statement
+			 */
+			break;
+		case d:
+		case 'F': System.out.print("not good");
+		}
+
+	}
 }

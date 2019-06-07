371dc91b3e098cdb104230bb603111029d00706a
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-07 10:12:15 -0700

    ROW_LOOP doesn't seem to output anything; not sure what was the takeaway

--- src/main/java/ocaprep/ch02/ReviewQuestion18.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion18.java
@@ -1,5 +1,19 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion18 {
+	/*
+	 * what is the output of the following code snippet?
+	 */
+	public static void main(String[] args) {
+		System.out.println("Review Question 18");
 
+		int count = 0;
+		ROW_LOOP: for (int row = 1; row <= 3; row++) {
+			for (int col = 1; col <= 2; col++) {
+				if (row * col % 2 == 0) continue ROW_LOOP;
+				count++;
+			}
+			System.out.println(count); // output: no output
+		}
+	}
 }

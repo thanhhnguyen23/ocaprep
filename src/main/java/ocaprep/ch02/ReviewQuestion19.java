9e0e01d89d8ad773c9eb511abf90c23dbd3694dc
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-07 10:12:58 -0700

    basic arithmetic operators and while loop

--- src/main/java/ocaprep/ch02/ReviewQuestion19.java
+++ src/main/java/ocaprep/ch02/ReviewQuestion19.java
@@ -1,5 +1,27 @@
 package ocaprep.ch02;
 
 public class ReviewQuestion19 {
+	/*
+	 * what is the result of the following code snippet?
+	 */
+
+	public static void main(String[] args) {
+
+		int m = 9, n = 1, x = 0;
+		while (m > n) {
+			m--;
+			n += 2;
+			x += m + n;
+		}
+		System.out.println(x); // output: 36
+
+	}
+	/*
+	 * summary
+	 * ------------------
+	 * m -> 9, 8, 7, 6
+	 * n -> 1, 3, 5, 7
+	 * x -> 0, 11, 23, 36
+	 */
 
 }

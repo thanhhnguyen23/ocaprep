ae01dc20e1a299fda2b01b717b3175c8056fea5a
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-08 16:50:28 -0700

    additional information about numerical promotion

---
+++ src/main/java/ocaprep/ch02/numericpromotion/NumericPromotionExample.java
@@ -0,0 +1,18 @@
+package ocaprep.ch02.numericpromotion;
+
+public class NumericPromotionExample {
+	/*
+	 * numeric promotion rules
+	 * 
+	 * 1. if two values have different data types, Java will automatically promote one of the values to the larger of the two data types
+	 * 2. if one of the values is integral and the other is floating-point, Java will automatically promote the integeral value to the floating-point value's data type
+	 * 3. smaller data types, namely byte, short, and char, are first promoted to int any time they're used with a Java binary arithmetic operator, even if neither of teh operands is int
+	 * 		3.1 byte
+	 * 		3.2 short
+	 * 		3.3 char
+	 * 4. after all promoition has occured and the operands have the same data type, the resulting value will have teh same data type as its promoted operands
+	 * p.102
+	 */
+	
+
+}

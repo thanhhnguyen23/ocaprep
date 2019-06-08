e3a8978ebd94fb49a25c56e85d84a16624ff3e3d
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-08 16:51:15 -0700

    additional notes on ordering of class elements

---
+++ src/main/java/ocaprep/ch01/classelements/ClassElementsExample.java
@@ -0,0 +1,32 @@
+/*
+ * ----------------------------
+ * ordering elements in a class
+ * ----------------------------
+ */
+
+/*
+ * 1. package declaration
+ *  	> not required
+ */
+package ocaprep.ch01.classelements;
+/*
+ * 2. import statements immediately placed after package declarations
+ * 		> not required
+ */
+
+/*
+ * 3. class declaration are placed after the import statements
+ * 		> required
+ */
+public class ClassElementsExample {
+
+	/*
+	 * 4. field declarations
+	 * 		> not required
+	 */
+	
+	/*
+	 * 5. method declarations 
+	 * 		> not required
+	 */
+}

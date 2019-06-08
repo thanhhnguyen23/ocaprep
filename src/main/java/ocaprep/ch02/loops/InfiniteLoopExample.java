f814fc7edc2b07c38b7197e782dcfd5602f3be0f
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-08 14:05:27 -0700

    adding infinite loop example

---
+++ src/main/java/ocaprep/ch02/loops/InfiniteLoopExample.java
@@ -0,0 +1,11 @@
+package ocaprep.ch02.loops;
+
+public class InfiniteLoopExample {
+
+	public static void main(String[] args) {
+		// infinite loop
+//		for ( ; ; ) {
+//			System.out.println("Hello World!");
+//		}
+	}
+}

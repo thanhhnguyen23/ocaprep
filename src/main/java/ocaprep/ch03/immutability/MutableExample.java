3ecfc3f87218f771d383b9ff45ae36f8d0c4e3d9
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-08 18:27:09 -0700

    adding notes for immutablility/mutability

--- src/main/java/ocaprep/ch03/immutability/MutableExample.java
+++ src/main/java/ocaprep/ch03/immutability/MutableExample.java
@@ -1,5 +1,15 @@
 package ocaprep.ch03.immutability;
 
 public class MutableExample {
+    private String s;
+    public void setS(String newS){ s = newS; } // setter makes it mutable
 
+    public String getS(){ return s; }
+}
+final class Immutable{
+    /*
+        immutable only has a getter
+     */
+    private String s = "name";
+    public String getS(){ return s; }
 }

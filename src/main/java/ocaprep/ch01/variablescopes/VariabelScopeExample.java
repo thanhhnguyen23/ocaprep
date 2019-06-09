a35975227ec62f3c914acc5976b3d61a91d6e796
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-08 17:01:08 -0700

    adding additional info about variable scopes

---
+++ src/main/java/ocaprep/ch01/variablescopes/VariabelScopeExample.java
@@ -0,0 +1,18 @@
+package ocaprep.ch01.variablescopes;
+
+public class VariabelScopeExample {
+	//TODO - find examples for different types of scopes
+
+	/*
+	 * class variables - in scope from declaration until program ends
+	 */
+
+	/*
+	 * local variable scope - in scope from declaration to end of block
+	 */
+
+	/*
+	 * instance variables - in scope from declaration until object garbage collected
+	 */
+	
+}

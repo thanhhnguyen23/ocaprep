a831ec7756d7ebc49403e93a65e1a802f0d537af
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-09 10:22:00 -0500

    fixed to standard formatting

--- src/main/java/ocaprep/ch03/immutability/MutableExample.java
+++ src/main/java/ocaprep/ch03/immutability/MutableExample.java
@@ -1,15 +1,29 @@
 package ocaprep.ch03.immutability;
 
 public class MutableExample {
-    private String s;
-    public void setS(String newS){ s = newS; } // setter makes it mutable
 
-    public String getS(){ return s; }
+	private String s;
+
+	// setter makes it mutable
+	public void setS(String newS) {
+		s = newS;
+	}
+
+	public String getS() {
+		return s;
+	}
 }
-final class Immutable{
-    /*
-        immutable only has a getter
-     */
-    private String s = "name";
-    public String getS(){ return s; }
+
+/*
+ * final modifier preserves immutability
+ */
+final class Immutable {
+	/*
+	 * immutable only has a getter
+	 */
+	private String s = "name";
+
+	public String getS() {
+		return s;
+	}
 }

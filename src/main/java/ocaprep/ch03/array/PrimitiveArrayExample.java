5abccde6b8bb93c1b4647fce55a10258958e28c0
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-09 16:42:37 -0500

    adding notes on primitive arrays

---
+++ src/main/java/ocaprep/ch03/array/PrimitiveArrayExample.java
@@ -0,0 +1,13 @@
+package ocaprep.ch03.array;
+
+public class PrimitiveArrayExample {
+
+	public static void main(String[] args) {
+		/*
+		 * type of array: int
+		 * required array symbol: []
+		 * size of array: [3]
+		 */
+		int[] numbers = new int[3]; // create array of type int named numbers, and initialize array size of 3
+	}
+}

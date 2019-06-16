96ee4013602c0480982656ad0f2945b824933dd0
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-15 22:40:32 -0500

    adding streams examples

---
+++ src/main/java/ocaprep/streams/ConvertStreamsToCollectionExample.java
@@ -0,0 +1,29 @@
+package ocaprep.streams;
+
+
+public class ConvertStreamsToCollectionExample {
+	// convert streams to other data structures
+	public static void main(String[] args) {
+//		example 1
+//		List<Integer> list = new ArrayList<Integer>();
+//		for(int i = 1; i < 10; i++) {
+//			list.add(i);
+//		}
+//		Stream<Integer> stream = list.stream();
+//
+//		List<Integer> evenNumbersList = stream.filter(i -> i % 2 == 0).collect(Collectors.toList());
+//		System.out.println(evenNumbersList);
+		
+
+//		example 2
+//		List<Integer> list = new ArrayList<Integer>();
+//		for(int i = 1; i < 10; i++) {
+//			list.add(i);
+//		}
+//		Stream<Integer> stream = list.stream();
+//		
+//		Integer [] evenNumbersArr = stream.filter(i -> i % 2 == 0).toArray(Integer[]::new);
+//		System.out.println(evenNumbersArr);
+		
+	}
+}
---
+++ src/main/java/ocaprep/streams/StreamExamples.java
@@ -0,0 +1,43 @@
+package ocaprep.streams;
+
+public class StreamExamples {
+	// building streams from collections api
+	
+	
+	public static void main(String[] args) {
+// 		example 1
+//		Stream<Integer> stream = Stream.of(1,2,3,4,5,6,7,8,9);
+//		stream.forEach(p -> System.out.println(p));
+		
+//		example 1.1
+//		Stream<String> stream = Stream.of("A", "B", "C");
+//		stream.forEach(p -> System.out.println(p));
+		
+//		example 2
+//		Stream<Integer> stream = Stream.of( new Integer[] {1,2,3,4,5,6,7,8,9} );
+//		stream.forEach(p -> System.out.println(p));
+		
+//		example 2.2
+//		Stream<String> stream = Stream.of( new String[] {"A", "B", "C" } );
+//		stream.forEach(p -> System.out.println(p));
+		
+		
+//		example 3
+//		List<Integer> list = new ArrayList<Integer>();
+//		for (int i = 1; i < 10; i++) {
+//			list.add(i);
+//		}
+//		Stream<Integer> stream = list.stream();
+//		stream.forEach(p -> System.out.println(p));
+		
+//		example 4
+//		Stream<Date> stream = Stream.generate(() -> { return new Date(); });
+//		stream.forEach(p -> System.out.println(p));
+		
+//		example 5
+//		IntStream stream = "12345_abcdefg".chars();
+//		stream.forEach(p -> System.out.println(p));
+		
+	}
+
+}

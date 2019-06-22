1cc836be6f6bc5e6d5e1ae5cca89c4d14822c2b7
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-21 22:24:55 -0500

    sort examples

--- src/main/java/ocaprep/ch03/sort/SortExample.java
+++ src/main/java/ocaprep/ch03/sort/SortExample.java
@@ -1,15 +1,27 @@
 package ocaprep.ch03.sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class SortExample {
 	// simple example sorts three numbers
 	public static void main(String[] args) {
-		int[] numbers = {6,9,1};
-		Arrays.sort(numbers);
-		
-		for (int i = 0; i < numbers.length; i++) {
-			System.out.println(numbers[i] + " ");
-		}
+		// example 1
+//		int[] numbers = {6,9,1};
+//		Arrays.sort(numbers);
+//		for (int i = 0; i < numbers.length; i++) {
+//			System.out.println(numbers[i] + " ");
+//		}
+
+		// example 2
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(99);
+		numbers.add(5);
+		numbers.add(81);
+
+		Collections.sort(numbers);
+		System.out.println(numbers); // 5, 81, 99
 	}
 }
